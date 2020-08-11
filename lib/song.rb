9ca2c214a6d909d9bb407b4acffd6113340da8e6
Kyle Stough <kstough00@gmail.com>
2020-08-11 15:44:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
-class Song
-  attr_accessor :name, :artist
+# class Song
+#   attr_accessor :name, :artist
   
-  def initialize(name)
-    self.name = name
-    self.artist = artist
-    @song = [] #so we can keep track of the songs
-  end
+#   def initialize(name)
+#     self.name = name
+#     self.artist = artist
+#     @song = [] #so we can keep track of the songs
+#   end
   
-  end
+#   end
