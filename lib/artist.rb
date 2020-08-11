dda7e1d48a28a119c88b0ff201f43a3d45c8a5bf
Kyle Stough <kstough00@gmail.com>
2020-08-11 15:38:52 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name, artist)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+  end
+  
+  
+  
+end
+
+
+
+
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    self.name = name
+    self.artist = artist
+    @song = [] #so we can keep track of the songs
+  end
+  
+  end
