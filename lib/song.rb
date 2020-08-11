48c3111251b3445252b050982e1f159f32f22f53
Kyle Stough <kstough00@gmail.com>
2020-08-11 17:28:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,10 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+  
+  def self.all
+    @@all
+  end
 
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
@@ -19,8 +23,5 @@ class Song
     song
   end
   
-  def self.all
-    @@all
-  end
   
 end
