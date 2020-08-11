a51f4d6b506bee03a235c0b53ac24754e439a689
Kyle Stough <kstough00@gmail.com>
2020-08-11 15:44:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,3 +8,18 @@
 #   end
   
 #   end
+
+class Song
+  attr_accessor :name, :artist, :file_name
+
+  def initialize(name)
+    @name = name
+    @artist = artist
+    @file_name = file_name
+  end
+
+  def self.new_by_filename(file_name)
+      self.name = file_name.split(" - ")[0]
+      self.artist.name = file_name.split(" - ")[1]
+  end
+end
