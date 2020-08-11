10fbbe08358c6696446dcde39b0d916f3454e3ba
Kyle Stough <kstough00@gmail.com>
2020-08-11 16:31:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,14 @@ class Song
     @name = name
   end
   
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
