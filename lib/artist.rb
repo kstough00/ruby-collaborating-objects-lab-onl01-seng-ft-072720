664ab5989dd4879f6cc3b9894dcc7aafa53acd35
Kyle Stough <kstough00@gmail.com>
2020-08-11 15:49:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,9 +40,9 @@ class Artist
     artist
   end
 
-  def print_songs #instance method iterates through @songs of an instance of Artist
-    self.songs.each {|song| song.name}
-  end
+  # def print_songs #instance method iterates through @songs of an instance of Artist
+  #   self.songs.each {|song| song.name}
+  # end
 end
 
 
